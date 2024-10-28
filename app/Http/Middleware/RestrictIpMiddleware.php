34603f1d015339777d2fb06d0424d5768afbd9ec
shahed141123 <khandkershahed23@gmail.com>
2024-10-28 23:44:13 +0600

    18-10-24

--- app/Http/Middleware/RestrictIpMiddleware.php
+++ app/Http/Middleware/RestrictIpMiddleware.php
@@ -21,11 +21,12 @@ class RestrictIpMiddleware
 
     public function handle(Request $request, Closure $next): Response
     {
-        $clientIp = $request->ip();
+        $apiKey = $request->header('X-API-KEY');
+        // dd($clientIp);
         // $allowedIps = Setting::latest('id')->first(['allowed_ip']);
         $settings = Setting::latest('id')->first();
         $allowedIps = collect(json_decode($settings->allowed_ip, true))->pluck('value')->toArray();
-        if (!in_array($clientIp, $allowedIps)) {
+        if (!in_array($apiKey, $allowedIps)) {
             return response()->json(['message' => 'Access Denied.'], 403);
         }
 
